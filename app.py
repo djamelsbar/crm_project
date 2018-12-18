7930b0a9d50d8d2bfdd25e218d0491ce42a92246
djamelsbar <45557798+djamelsbar@users.noreply.github.com>
2018-12-18 17:01:21 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,42 +1,49 @@
-
-from flask import Flask, render_template, request, flash,url_for,redirect
+from flask import Flask
 from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-
+from flask_admin.contrib.sqla import ModelView, filters
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'admin.db')
 
 app.config['SECRET_KEY'] = 'mysecret'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 admin = Admin(app)
 
 from model import *
 
 
-class UserView(ModelView):
-    #can_delete = False
-    pass
+class ProspectModelView(ModelView):
+    inline_models = (Note,)
+    column_filters = ('name', 'phone','email')
+    form_excluded_columns = ['projects']
 
 
-admin.add_view(UserView(User, db.session))
+class NoteModelView(ModelView):
+    form_excluded_columns = ['prospect']
 
-admin.add_view(ModelView(Project, db.session))
 
-admin.add_view(ModelView(Procpect, db.session))
+class ProjectModelView(ModelView):
+    form_excluded_columns = ['prospects']
 
-admin.add_view(ModelView(Note, db.session))
 
-admin.add_view(ModelView(procpect_project, db.session))
+class ProspectProjectModelView(ModelView):
+    column_filters = ('status', 'prospect.name','project.title','date')
+
+
+admin.add_view(ModelView(User, db.session))
+
+admin.add_view(ProjectModelView(Project, db.session))
+
+admin.add_view(ProspectModelView(Prospect, db.session))
+
+admin.add_view(NoteModelView(Note, db.session))
+
+admin.add_view(ProspectProjectModelView(ProspectProject, db.session))
 
--- model.py
+++ model.py
@@ -1,70 +1,63 @@
 import enum
 
+from sqlalchemy.orm import relationship
+
 from app import db
 from datetime import datetime
 
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	login = db.Column(db.String)
-	password = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String)
+    password = db.Column(db.String)
 
 
-class Status_type(enum.Enum):
-	visit = "visit realized"
-	purchase_sponsor = "purchase made at the promoter"
-	purchase_other = "purchase made elsewhere"
-	contact = "to recontact"
+class StatusType(enum.Enum):
+    visit = "visit realized"
+    purchase_sponsor = "purchase made at the promoter"
+    purchase_other = "purchase made elsewhere"
+    contact = "to recontact"
 
 
-# class Status(db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	status_type = db.Column(db.Enum(Status_type))
+class ProspectProject(db.Model):
 
-
-procpect_project = db.Table('procpect_project',
-                                db.Column('procpect_id', db.Integer, db.ForeignKey('procpect.id'), primary_key=True),
-                                db.Column('project_id', db.Integer, db.ForeignKey('project.id'), primary_key=True)
-								,db.Column('status_type' , db.Enum(Status_type))
-
-)
+    procpect_id = db.Column(db.Integer, db.ForeignKey('prospect.id'), primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), primary_key=True)
+    status = db.Column('status_type', db.Enum(StatusType))
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    prospect = relationship('Prospect', back_populates='projects')
+    project = relationship('Project', back_populates='prospects')
 
 
 class Project(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String,nullable=False)
-	description = db.Column(db.String, nullable=False)
-	wilaya = db.Column(db.String)
-	city = db.Column(db.String)
-
-# class Status(db.Model) :
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	description = db.Column(db.String, nullable=False)
-# 	procpect_id = db.Column(db.Integer, db.ForeignKey('procpect.id'), nullable=False)
-# 	procpect = db.relationship('Procpect', backref=db.backref('status', lazy=True))
-# 	project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
-# 	project = db.relationship('Project', backref=db.backref('status', lazy=True))
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    wilaya = db.Column(db.String)
+    city = db.Column(db.String)
+    prospects = relationship('ProspectProject', back_populates='project')
+    def __repr__(self):
+        return self.title
 
 
-class Procpect(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String,nullable=False)
-	last_name = db.Column(db.String)
-	phone = db.Column(db.Integer, nullable=False)
-	email = db.Column(db.String)
-	date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-	project = db.relationship("Project", secondary=procpect_project )
+class Prospect(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String)
+    phone = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    projects = relationship('ProspectProject', back_populates='prospect')
+    notes = db.relationship('Note', backref='prospect', lazy=True)
 
-
-
+    def __repr__(self):
+        return self.name
 
 
 class Note(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	description = db.Column(db.String, nullable=False)
-	date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-	procpect_id = db.Column(db.Integer, db.ForeignKey('procpect.id'), nullable=False)
-	procpect = db.relationship('Procpect', backref=db.backref('note', lazy=True))
-
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    prospect_id = db.Column(db.Integer, db.ForeignKey('prospect.id'), nullable=False)
 
 
